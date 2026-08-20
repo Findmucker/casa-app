af98068fa92607796196af2d2e42cdaeedd302f1
Findmucker <findmuckah@gmail.com>
2026-08-20 17:12:06 +0100

    refactor(android): remove obsolete repository shim

--- android/app/src/main/java/com/findmucker/casa/RepositoryExtensions.kt
+++
@@ -1,9 +0,0 @@
-package com.findmucker.casa
-
-@Suppress("UNUSED_PARAMETER")
-suspend fun FirebaseCasaRepository.toggleItem(
-    houseId: String,
-    actorName: String,
-    section: HouseSection,
-    item: HouseItem,
-) = toggleItem(houseId, section, item)
